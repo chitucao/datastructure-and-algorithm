44f8e7d6641397aac8f0d333f268edfae796dd99
dennyfly <2469589812@qq.com>
2021-11-22 18:10:09 +0800

    不带虚拟头节点的单链表查询和删除

--- datastructure/src/main/java/top/dennyfly/datastructure/algo/A02_LinkedList/LinkedList.java
+++ datastructure/src/main/java/top/dennyfly/datastructure/algo/A02_LinkedList/LinkedList.java
@@ -6,7 +6,14 @@ import java.util.Objects;
  * @author dennyfly
  * @since 2021/11/19 17:15
  * 实现单链表，支持增删操作
- * #带头节点，不维护size变量，支持按index操作
+ * #不带虚拟头节点，不维护size变量，支持按index操作
+ * <p>
+ * #基本操作#
+ * findByValue  根据值查询
+ * findByIndex  根据索引查询
+ * removeByValue    根据value删除
+ * removeByNode     根据node
+ * removeByIndex    根据索引删除
  */
 public class LinkedList<E> {
 
@@ -25,16 +32,17 @@ public class LinkedList<E> {
         }
     }
 
-
     private Node head;
 
     public LinkedList() {
         this.head = new Node(null);
     }
 
-    // 根据value获取节点
+    /**
+     * 根据value获取
+     */
     public Node findByValue(E value) {
-        Node p = head.next;
+        Node p = head;
         while (p != null && !Objects.equals(p.value, value)) {
             p = p.next;
         }
@@ -42,26 +50,93 @@ public class LinkedList<E> {
     }
 
     /**
-     * 根据value删除节点
+     * 根据index获取
+     */
+    public Node findByIndex(int index) {
+        Node p = head;
+        int pos = 0;
+        while (p != null && index != pos) {
+            p = p.next;
+            pos++;
+        }
+        return p;
+    }
+
+
+    /**
+     * 根据value删除
      * #需遍历整个链表，找到待删除节点后需要断开待删除节点的next
      */
     public void removeByValue(E value) {
-        Node pre = head;
-
-        // 注意循环是遍历链表所有元素，也就是pre.next!=null
-        while (pre.next != null) {
-            if (Objects.equals(pre.next.value, value)) {
-                break;
-            }
-            pre = pre.next;
+        // 链表为空直接返回
+        if (head == null) {
+            return;
         }
 
-        if (pre.next != null) {
-            Node delNode = pre.next;
-            pre.next = delNode.next;
-            delNode.next = null;
+        // 考虑head节点为所查找节点的情况
+        if (Objects.equals(value, head.value)) {
+            head = head.next;
+        }
+
+        // 外层遍历，内层比较退出循环
+        Node prev = head;
+        while (prev.next != null) {
+            if (Objects.equals(value, prev.next.value)) {
+                break;
+            }
+            prev = prev.next;
+        }
+
+        if (prev.next != null) {
+            Node tmp = prev.next;
+            prev.next = tmp.next;
+            tmp.next = null;
         }
     }
 
+    /**
+     * 根据节点删除
+     */
+    public void removeByNode(Node node) {
+        if (head == null || node == null) {
+            return;
+        }
+
+        // 考虑head节点为所查找节点的情况
+        if (Objects.equals(head, node)) {
+            head = head.next;
+        }
+
+        Node prev = head;
+        while (prev.next != null) {
+            if (Objects.equals(node, prev.next)) {
+                break;
+            }
+            prev = prev.next;
+        }
+
+        if (prev.next != null) {
+            Node tmp = prev.next;
+            prev.next = tmp.next;
+            tmp.next = null;
+        }
+    }
+
+    /**
+     * 根据索引删除
+     */
+    public void removeByIndex(int index) {
+        if (index == 0) {
+            head = head.next;
+        }
+        Node node = findByIndex(index - 1);
+        if (node == null) {
+            return;
+        }
+
+        Node tmp = node.next;
+        node.next = tmp.next;
+        tmp = null;
+    }
 
 }
