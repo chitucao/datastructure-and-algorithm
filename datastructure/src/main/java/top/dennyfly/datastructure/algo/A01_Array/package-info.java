1b5f87ed7b0dbc1311608c072a290cd4b39e9e49
dennyfly <2469589812@qq.com>
2021-11-23 13:11:40 +0800

    add with non dummyhead LinkedList

---
+++ datastructure/src/main/java/top/dennyfly/datastructure/algo/A01_Array/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * 数组问题
+ * 1.合并两个有序数组
+ * 2.
+ */
+package top.dennyfly.datastructure.algo.A01_Array;
---
+++ datastructure/src/main/java/top/dennyfly/datastructure/algo/A02_LinkedList/Main.java
@@ -0,0 +1,66 @@
+package top.dennyfly.datastructure.algo.A02_LinkedList;
+
+import org.junit.Test;
+
+/**
+ * @author dennyfly
+ * @since 2021/11/23 9:44
+ */
+public class Main {
+    private static LinkedList<Integer> linkedList;
+
+    static {
+        linkedList = new LinkedList<>();
+        for (int i = 0; i < 5; i++) {
+            linkedList.addLast(i);
+        }
+    }
+
+
+    @Test
+    public void testLinkedList() {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for (int i = 0; i < 5; i++) {
+            linkedList.addLast(i);
+        }
+        System.out.println(linkedList);
+        for (int i = 0; i < 3; i++) {
+            linkedList.addFirst(i);
+        }
+        System.out.println(linkedList);
+
+        linkedList.removeByIndex(2);
+        System.out.println(linkedList);
+
+        linkedList.removeByValue(4);
+        System.out.println(linkedList);
+        ;
+
+        linkedList.addByIndex(2, 7);
+        System.out.println(linkedList);
+
+        linkedList.set(0, 12);
+        System.out.println(linkedList);
+
+        linkedList.removeFirst();
+        System.out.println(linkedList);
+
+        linkedList.removeLast();
+        System.out.println(linkedList);
+    }
+
+    @Test
+    public void testReverseNode() {
+        linkedList.reverseNode1();
+        System.out.println(linkedList);
+        linkedList.reverseNode2();
+        System.out.println(linkedList);
+    }
+
+    @Test
+    public void testMidNode() {
+        System.out.println(linkedList);
+        LinkedList<Integer>.Node node = linkedList.midNode();
+        System.out.println("中间节点：" + node.value);
+    }
+}
